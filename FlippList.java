d6a881e37c2642a8e97e983b41a9538099d01eee
Sergey Yakovlev <pcs_zero@inbox.ru>
2022-12-21 21:10:46 +0300

    task1

---
+++ FlippList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class FlippList {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<String>();
+        list.add("Horse");
+        list.add("Dog");
+        list.add("Cat");
+        list.add("Goat");
+        list.add("Cow");
+        System.out.println(list);
+
+    }
+}
